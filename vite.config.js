c5a1439f0d8cd5361c73f7c2f65a932c7084a225
Swarnabha700 <chakrabortyswarnabha700@gmail.com>
2025-02-15 10:11:46 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Secret-Project/",
+  base: "/Secret-Project-main/",
   plugins: [react()],
 })
